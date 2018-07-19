cb389f2dcc8f6423ab4a97b2ee058d62bbdf0c9c
orendavi <oren.davis@gmail.com>
2018-07-19 16:01:38 -0400

    New branch

--- app.py
+++ app.py
@@ -1,22 +1,14 @@
 # from models.post import Post
 from database import Database
 from models.blog import Blog
+from menu import Menu
 
 
 
 
 Database.initialize()
 
-blog = Blog(author="Oren",
-            title="Another great post",
-            description="Sample description")
+menu = Menu()
 
 
-blog.new_post()
-
-blog.save_to_mongo()
-
-from_database = Blog.from_mongo(blog.id)
-
-print(blog.get_posts())
-
+menu.run_menu()
---
+++ app_save_b4_menu.py
@@ -0,0 +1,23 @@
+# from models.post import Post
+from database import Database
+from models.blog import Blog
+from menu import Menu
+
+
+
+
+Database.initialize()
+
+blog = Blog(author="Oren",
+            title="Another great post",
+            description="Sample description")
+
+
+blog.new_post()
+
+blog.save_to_mongo()
+
+from_database = Blog.from_mongo(blog.id)
+
+print(blog.get_posts())
+
